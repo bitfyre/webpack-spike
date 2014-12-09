cba8fb17cf3464c4b65da05e84c8c391e37ab925
Alex Lemanski <alex.lemanski@bitfyre.net>
2014-12-08 22:09:25 -0800

    Define bundle directory as build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+var path = require('path');
+
 module.exports = {
     entry: "./entry.js",
     output: {
-        path: __dirname,
+        path: path.join(__dirname, 'build'),
         filename: "bundle.js"
     },
     module: {
