21ae8e146a105635f47424a48eda759f666a8359
Alex Lemanski <alex.lemanski@bitfyre.net>
2014-12-19 11:19:17 -0800

    Consistency betwee ' and "

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,20 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  entry: "./source/js/app.js",
+  entry: path.join(__dirname, 'source/js/app.js'),
   output: {
     path: path.join(__dirname, 'build'),
-    filename: "app.min.js"
+    filename: 'app.min.js'
   },
   module: {
     loaders: [
       {
         test: /\.css$/,
-        loader: "style!css"
+        loader: 'style!css'
       },
       {
         test: /\.js$/,
-        loader: "es6-loader"
+        loader: 'es6-loader'
       }
     ]
   },
